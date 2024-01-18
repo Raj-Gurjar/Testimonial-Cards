793d60f7c4cc9acf19cdf4d9df17a356eef6af3b
Raj-Gurjar <20ai1045@mitsgwl.ac.in>
2024-01-18 17:32:04 +0530

    logic correction

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,28 +1,19 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import Card from "./Card";
 
 export default function Cards({ data }) {
-  // console.log(data.length);
-
   let [index, setIndex] = useState(0);
 
   function prevHandler() {
-    console.log(index);
-    if (index === 0) {
-      index = data.length;
-    }
-    setIndex(index - 1);
+    console.log("Previous Index:", index); //previous index will be printed not current.
+    setIndex((prevIndex) => (prevIndex === 0 ? data.length - 1 : prevIndex - 1));
+  }
 
-  }
   function nxtHandler() {
-    console.log(index);
-    if (index === data.length - 1) {
-      index = -1;
-    }
-    setIndex(index + 1);
+    setIndex((prevIndex) => (prevIndex === data.length - 1 ? 0 : prevIndex + 1));
   }
+
   function ranHandler() {
-    console.log(index);
     let ranInd = Math.floor(Math.random() * data.length);
     setIndex(ranInd);
   }
@@ -40,23 +31,22 @@ export default function Cards({ data }) {
   return (
     <div className='cards-cls'>
       <h2>Cards</h2>
-
       <div className='cards-cntnr'>
-
 
         <Card key={data[index].id} personData={data[index]} />
 
-
         <div className="btn-cntnr">
+
           <button className='prev' onClick={prevHandler}>-</button>
           <button className='nxt' onClick={nxtHandler}>+</button>
+
         </div>
+
         <div className="shuffle">
           <button onClick={ranHandler}>Shuffle</button>
         </div>
 
       </div>
-
     </div>
-  )
+  );
 }
